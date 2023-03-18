2d82748b611ea9bf975a1903a9d2802d15da5dca
Herbert Li <2729415988@qq.com>
2023-03-19 01:20:53 +0800

    delete some lines

--- week5.R
+++ week5.R
@@ -1,16 +1,4 @@
 library(dplyr)
-library(nycflights13)
-by_tailnum <- group_by(flights, tailnum)
-delay <- summarise(by_tailnum,
-                   count = n(),
-                   dist = mean(distance, na.rm = TRUE),
-                   delay = mean(arr_delay, na.rm = TRUE))
-arrange(delay,desc(count))
-delay.sub <- filter(delay, count > 20, dist < 2000)
-delay.sub
-
-
-library(dplyr)
 iris.type <- group_by(iris,Species)
 iris.mean <- summarise(iris.type,
                        Sepal.Length = mean(Sepal.Length),
@@ -21,5 +9,4 @@ library(ggplot2)
 ggplot(iris,aes(x = Sepal.Length,y = Sepal.Width, col = Species))+
   geom_point()+
   geom_point(data = iris.mean,shape = 15,size = 5)
-ggplot()
 
